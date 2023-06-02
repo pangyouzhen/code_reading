bbcf4d636d57f0354b23a76c178289443b92a7cc
pang <pangyouzhen@live.com>
2023-06-02 19:21:07 +0800

    fixed

--- dl/multiattention.py
+++ dl/multiattention.py
@@ -1,44 +1,31 @@
+import math
 from typing import List, Optional, Tuple
 
 import torch
-import torch.nn.functional as F
 from torch import Tensor
 from torch.nn import Module
+from torch.nn.functional import linear, softmax
 from torch.nn.init import constant_, xavier_uniform_
 from torch.nn.modules.linear import NonDynamicallyQuantizableLinear
 from torch.nn.parameter import Parameter
-from torch.nn.functional import softmax, linear
-import math
 
 
 class MultiheadAttention1(Module):
-    __constants__ = ['batch_first']
-    bias_k: Optional[torch.Tensor]
-    bias_v: Optional[torch.Tensor]
 
-    def __init__(self, embed_dim, num_heads, dropout=0., bias=True, add_bias_kv=False, add_zero_attn=False,
-                 kdim=None, vdim=None, batch_first=False, device=None, dtype=None) -> None:
-        factory_kwargs = {'device': device, 'dtype': dtype}
+    def __init__(self, embed_dim, num_heads, bias=True, batch_first=False,) -> None:
         super().__init__()
-        self.embed_dim = embed_dim
-        self.kdim = kdim if kdim is not None else embed_dim
-        self.vdim = vdim if vdim is not None else embed_dim
-        self._qkv_same_embed_dim = self.kdim == embed_dim and self.vdim == embed_dim
         self.num_heads = num_heads
-        self.dropout = dropout
         self.batch_first = batch_first
         self.head_dim = embed_dim // num_heads
         self.in_proj_weight = Parameter(torch.empty(
-            (3 * embed_dim, embed_dim), **factory_kwargs))
+            (3 * embed_dim, embed_dim)))
         self.register_parameter('q_proj_weight', None)
         self.register_parameter('k_proj_weight', None)
         self.register_parameter('v_proj_weight', None)
         self.in_proj_bias = Parameter(
-            torch.empty(3 * embed_dim, **factory_kwargs))
+            torch.empty(3 * embed_dim))
         self.out_proj = NonDynamicallyQuantizableLinear(
-            embed_dim, embed_dim, bias=bias, **factory_kwargs)
-        self.bias_k = self.bias_v = None
-        self.add_zero_attn = add_zero_attn
+            embed_dim, embed_dim, bias=bias)
         self._reset_parameters()
 
     def _reset_parameters(self):
@@ -50,28 +37,14 @@ class MultiheadAttention1(Module):
             self,
             query: Tensor,
             key: Tensor,
-            value: Tensor,
-            key_padding_mask: Optional[Tensor] = None,
-            need_weights: bool = True,
-            attn_mask: Optional[Tensor] = None,
-            average_attn_weights: bool = True,
-            is_causal: bool = False) -> Tuple[Tensor, Optional[Tensor]]:
-        is_batched = query.dim() == 3
+            value: Tensor,) -> Tuple[Tensor, Optional[Tensor]]:
+        query.dim() == 3
 
-        why_not_fast_path = ''
-        why_not_fast_path = "training is enabled"
-        any_nested = query.is_nested or key.is_nested or value.is_nested
         attn_output, attn_output_weights = multi_head_attention_forward(
-            query, key, value, self.embed_dim, self.num_heads,
+            query, key, value, self.num_heads,
             self.in_proj_weight, self.in_proj_bias,
-            self.bias_k, self.bias_v, self.add_zero_attn,
-            self.dropout, self.out_proj.weight, self.out_proj.bias,
-            training=self.training,
-            key_padding_mask=key_padding_mask,
-            need_weights=need_weights,
-            attn_mask=attn_mask,
-            average_attn_weights=average_attn_weights,
-            is_causal=is_causal)
+            self.out_proj.weight, self.out_proj.bias,
+            )
         return attn_output, attn_output_weights
 
 
@@ -79,35 +52,15 @@ def multi_head_attention_forward(
     query: Tensor,
     key: Tensor,
     value: Tensor,
-    embed_dim_to_check: int,
     num_heads: int,
     in_proj_weight: Optional[Tensor],
     in_proj_bias: Optional[Tensor],
-    bias_k: Optional[Tensor],
-    bias_v: Optional[Tensor],
-    add_zero_attn: bool,
-    dropout_p: float,
     out_proj_weight: Tensor,
     out_proj_bias: Optional[Tensor],
-    training: bool = True,
-    key_padding_mask: Optional[Tensor] = None,
-    need_weights: bool = True,
-    attn_mask: Optional[Tensor] = None,
-    use_separate_proj_weight: bool = False,
-    q_proj_weight: Optional[Tensor] = None,
-    k_proj_weight: Optional[Tensor] = None,
-    v_proj_weight: Optional[Tensor] = None,
-    static_k: Optional[Tensor] = None,
-    static_v: Optional[Tensor] = None,
-    average_attn_weights: bool = True,
-    is_causal: bool = False,
 ) -> Tuple[Tensor, Optional[Tensor]]:
     tgt_len, bsz, embed_dim = query.shape
     src_len, _, _ = key.shape
-    other_name = "attn_mask",
-    target_type = query.dtype
     head_dim = embed_dim // num_heads
-    assert in_proj_weight is not None, "use_separate_proj_weight is False but in_proj_weight is None"
 
     def _in_projection_packed(
         q: Tensor,
@@ -149,11 +102,11 @@ if __name__ == "__main__":
     embed_dim = 512
     num_heads = 8
 
-    query = torch.randn(32,10,embed_dim)
-    key,value = query,query
+    query = torch.randn(32, 10, embed_dim)
+    key, value = query, query
 
-    multihead_attn = MultiheadAttention1(embed_dim,num_heads)
-    attn_outputs, attn_output_weights = multihead_attn(query,key,value)
+    multihead_attn = MultiheadAttention1(embed_dim, num_heads)
+    attn_outputs, attn_output_weights = multihead_attn(query, key, value)
 
     print(attn_outputs)
     print(attn_output_weights)
